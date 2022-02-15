8cbb0e30116517bb3e7b95bcd68a7cea0d1647d9
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-02-15 10:20:52 -0800

    Update index.sh

--- scripts/api/index.sh
+++ scripts/api/index.sh
@@ -6,7 +6,7 @@ apk add tree
     agent -ver
 } || {
     printf "\nmissing agent. begin agent installation\n\n"
-    source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64_conf.txt)
+    source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64.txt) -ver
 }
 
 mkdir -p ~/.ec/scripts
